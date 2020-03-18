eacc566fc24c796b7fb1a3f6d6d073fd2ee1e1cd
Donni <donnikds@gmail.com>
2020-03-18 23:00:54 +0300

    Homework7 Client Chat

---
+++ NetworkClient/src/ru/geekbrains/java2/client/view/ClientChat.java
@@ -0,0 +1,70 @@
+package ru.geekbrains.java2.client.view;
+
+import ru.geekbrains.java2.client.controller.ClientController;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ClientChat extends JFrame {
+
+    private JPanel mainPanel;
+    private JList<String> usersList;
+    private JTextField messageTextField;
+    private JButton sendButton;
+    private JTextArea chatText;
+
+    private ClientController controller;
+
+    public ClientChat(ClientController controller) {
+        this.controller = controller;
+        setTitle(controller.getUsername());
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setContentPane(mainPanel);
+        addListeners();
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                controller.shutdown();
+            }
+        });
+    }
+
+
+    private void addListeners() {
+        sendButton.addActionListener(e -> ClientChat.this.sendMessage());
+        messageTextField.addActionListener(e -> sendMessage());
+    }
+
+    private void sendMessage() {
+        String message = messageTextField.getText().trim();
+        if (message.isEmpty()) {
+            return;
+        } else if (message.startsWith("/w")){
+            String[] s = message.split("\\s+", 3);
+            appendOwnMessageForAnotherUser(s[2], s[1]);
+        } else {
+            appendOwnMessage(message);
+        }
+
+        controller.sendMessage(message);
+        messageTextField.setText(null);
+    }
+
+    public void appendMessage(String message) {
+        SwingUtilities.invokeLater(() -> {
+            chatText.append(message);
+            chatText.append(System.lineSeparator());
+        });
+    }
+
+
+    private void appendOwnMessage(String message) {
+        appendMessage("Я: " + message);
+    }
+
+    private void appendOwnMessageForAnotherUser(String message, String nickname) {
+        appendMessage("Личное сообщение для " + nickname + " :" + message);
+    }
+}
